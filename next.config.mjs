3899c64fd456167dc00b8f61248a39af1a79f097
Suryakanta Mohapatra <neural.surya@gmail.com>
2026-08-10 14:46:59 -0700

    Remove legacy root app files

--- next.config.mjs
+++
@@ -1,7 +0,0 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  typedRoutes: true,
-  outputFileTracingRoot: "/Users/suryakanta/Desktop/hacks/webinars/skrutai-webinar-guardian"
-};
-
-export default nextConfig;
